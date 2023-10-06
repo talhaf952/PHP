305f45c354e320df941981b6c790cc753af0a087
Muhammad Talha <127777461+talhaf952@users.noreply.github.com>
2023-10-06 11:32:30 -0600

    Update phpObject.php

--- phpObject.php
+++ phpObject.php
@@ -3,24 +3,25 @@ class Car
 {
  public $color;
  public $model;
+ public $look;
  
- public function __construct($color,$model)
+ public function __construct($color,$model,$look)
  {
      $this->color=$color;
      $this->model=$model;
+     $this->look=$look;
  }
  public function message()
  {
-    return  "My car color is ". $this->color. " and model is " . $this->model ." !";
+    return  "My car color is ". $this->color. ", model is " . $this->model . " and " . $this->look . " look." ;
  }
 }
-$obj1=new Car("Red", " Honda");
+$obj1=new Car("Red", " Honda", " Good");
 echo $obj1->message();
 
 echo "\n";
 
-$obj2 =new Car("White", "Suzuki");
+$obj2 =new Car("White", "Suzuki", " New");
 echo $obj2->message();
-
 
 ?>
