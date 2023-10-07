d9d8d8514351b8c1ea960d11cf550d70b1926010
Muhammad Talha <127777461+talhaf952@users.noreply.github.com>
2023-10-07 10:40:10 -0600

    Update stringFunctions.php

--- stringFunctions.php
+++ stringFunctions.php
@@ -12,6 +12,9 @@ echo"\n";
 echo strrev("A B C D E F G H I J K L M N O P Q R S T U V W X Y Z");
 echo"\n";
 
+echo strrev("1 2 3 4 5 6 7 8 9 10");
+echo"\n";
+
 //strpos() - Search For a Text Within a String
 echo strpos("Hi! I am Muhammad Talha Farooq.","Muhammad");
 echo"\n";
