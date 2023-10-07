f89f51b40fdaa18432506839fff469349fc5ad27
Muhammad Talha <127777461+talhaf952@users.noreply.github.com>
2023-10-07 10:38:49 -0600

    stringFunctions.php

---
+++ stringFunctions.php
@@ -0,0 +1,23 @@
+<?php
+//PHP String Functions
+//strlen()    returns string length
+echo strlen("Hi! I am Talha.");
+echo "\n";
+
+//The PHP str_word_count() function counts the number of words in a string.
+echo str_word_count("Hello! I am MUhammad Talha Farooq.");
+echo"\n";
+
+//strrev() - Reverse a String
+echo strrev("A B C D E F G H I J K L M N O P Q R S T U V W X Y Z");
+echo"\n";
+
+//strpos() - Search For a Text Within a String
+echo strpos("Hi! I am Muhammad Talha Farooq.","Muhammad");
+echo"\n";
+
+//str_replace() - Replace Text Within a String
+echo str_replace("M.","Muhammad","Hello! I am M. Talha Farooq");
+echo "\n";
+
+?>
