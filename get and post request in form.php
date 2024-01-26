52a1ee2c095bb452fe90f3b25733e5adfbea1fae
Muhammad Talha <127777461+talhaf952@users.noreply.github.com>
2024-01-26 22:13:39 +0500

    Update get and post request in form.php

--- get and post request in form.php
+++ get and post request in form.php
@@ -1,6 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>This is Student Management. </title>
+    <style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover:not(.active) {
+  background-color: #111;
+}
+
+.active {
+  background-color: #04AA6D;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+input[type=text], select, textarea {
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  resize: vertical;
+}
+
+label {
+  padding: 12px 12px 12px 0;
+  display: inline-block;
+}
+
+input[type=submit] {
+  background-color: #04AA6D;
+  color: white;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  float: right;
+}
+
+input[type=submit]:hover {
+  background-color: #45a049;
+}
+
+.container {
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+}
+
+.col-25 {
+  float: left;
+  width: 25%;
+  margin-top: 6px;
+}
+
+.col-75 {
+  float: left;
+  width: 75%;
+  margin-top: 6px;
+}
+
+/* Clear floats after the columns */
+.row::after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+/* Responsive layout - when the screen is less than 600px wide, make the two columns stack on top of each other instead of next to each other */
+@media screen and (max-width: 600px) {
+  .col-25, .col-75, input[type=submit] {
+    width: 100%;
+    margin-top: 0;
+  }
+}
+</style>
+</head>
+<body>
+<ul>
+  <li><a href="/">Home</a></li>
+  <li><a href="#news">News</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li style="float:right"><a class="active" href="#about">About</a></li>
+</ul>
 <div class="container">
 <h2>Please Enter your Name and Email.</h2>
-<form action="index.php" method="post">
+<form action="get and post request in form.php" method="post">
   <div class="form_group">
     <label for="email">Email Addresss</label>
     <input type="email" name="email" id="email">
@@ -11,7 +118,8 @@
   </div>
   <button type="submit">Submit</button>
 </form>
-
+</div>
+</div>
 <?php
   if ($_SERVER['REQUEST_METHOD']=='POST')
   {
@@ -24,63 +132,5 @@
   }
 ?>
 
-
-
-
-
-
-
-
-
-
-#include<iostream>
-using namespace std;
-int main(){
-    cout<<"Here we have Multidimensional Array of 0 and 1. We have to find ships and count them.\n";
-    cout<<"1. Find number of ships available.\n";
-    cout<<"2. Find coordinate of each ship found.\n"<<endl;
-    int ships[4][4]={
-        {0,1,0,0},
-        {0,0,1,0},
-        {1,0,0,1},
-        {1,0,1,0}
-    };
-    int count=0;int pointer;
-    for(int i=0;i<4;i++)
-    {
-        for(int j=0;j<4;j++)
-        {
-            if(ships[i][j]==1)
-            {
-                count++;
-                cout<<count<<" Ship found at index ["<<i<<"]["<<j<<"].\n";
-            }
-        }
-    }
-    cout<<"\nThere are "<<count<<" Ships in this area."<<endl;
-}
-
-
-
-
-
-=============================
-
-
-  echo "Here is next work.";
-    $servername="localhost";
-    $username="root";
-    $password="123";
-
-    $conn=mysqli_connect($servername,$username,$password);
-    if(!$conn)
-    {
-      die("We are sorry. not connected.".mysqli_connect_errno());
-    }
-    else{
-    echo "Connection was successful";
-    }
-
-
-==============================
-
+</body>
+</html>
